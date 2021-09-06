a2d7da2cbcccc3c0fbc29b990deaae1fb014babe
xiaotian.fang <fxt1998@gmail.com>
2021-09-06 22:19:06 +0800

    update 0906

--- back_end/resetful-web-services/src/main/java/com/xiaotian/rest/webservices/resetfulwebservices/jwt/resource/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.xiaotian.rest.webservices.resetfulwebservices.jwt.resource;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
