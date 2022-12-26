1b848f5039a633d920d8863e903f601e52b0c52d
x1aotian <fangxt@uw.edu>
2022-12-26 15:16:38 -0800

    1226 backend update

---
+++ restful-web-services/target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'x1aotian', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'x1aotian', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'x1aotian', 'Learn Microservices', sysdate(), false);
