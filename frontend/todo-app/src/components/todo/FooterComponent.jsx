b8ee14da1cb919c11d5e08f5d92829cf196916d4
x1aotian <fangxt@uw.edu>
2022-12-26 15:08:53 -0800

    1226 fronted update

---
+++ frontend/todo-app/src/components/todo/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FooterComponent extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                <span className="text-muted">All Rights Reserved 2020 @x1aotian</span>
+            </footer>
+        )
+    }
+}
+
+export default FooterComponent
